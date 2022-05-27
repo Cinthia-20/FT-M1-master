a76120d3915337e36ef92140ba6424fe32475bfa
Cinthia-20 <estaycinthia@gmail.com>
2022-05-27 12:11:09 -0300

    Homework M1 IntroToCS

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,13 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let total = 0;
+  for(let i =0; i < num.length; i++){
+    total = total + num[i] * Math.pow(2, num.length -1 -i); 
+  }
+  return total;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let str= "" ;
+  while (num>0){
+    let resto=num%2;
+    str=resto+str;
+    num=Math.floor(num/2)
+  }
+  return str;
+ }
 
-}
 
 
 module.exports = {
